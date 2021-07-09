f3e05f6719bd7bc3214d0b6355c0735f39fc0d28
Tomas Torres <tomas8torres@gmail.com>
2021-07-08 22:30:50 -0500

    sign in form scaffold

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -6,26 +6,64 @@ import AuthLayout from "../../layouts/AuthLayout";
 import Routes from "../../Routes";
 import { FC } from "react";
 import { Link } from "react-router-dom";
-import { Button, Row, Typography } from "antd";
+import { Button, Row, Form, Input } from "antd";
+import { MailOutlined, LockOutlined } from "@ant-design/icons";
 
-const { Text } = Typography;
+const SignInPage: FC = () => {
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
 
-const SignInPage: FC = () => (
-  <AuthLayout>
-    <Row justify="center">
-      <AppTitle />
-    </Row>
-    <Row justify="center">
-      <Text>Sign In!</Text>
-    </Row>
-    <Row justify="center">
-      <Link to={Routes.signUp}>
-        <Button>
-          <Text>Sign Up Here</Text>
-        </Button>
-      </Link>
-    </Row>
-  </AuthLayout>
-);
+  return (
+    <AuthLayout>
+      <Row justify="center">
+        <AppTitle />
+      </Row>
+      <Row justify="center">
+        <Form
+          name="sign-in"
+          className="sign-in-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: "Please input your Username!" }]}
+          >
+            <Input
+              prefix={<MailOutlined className="site-form-item-icon" />}
+              placeholder="Email"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your Password!" }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Link to={Routes.signIn}>Forgot Password?</Link>
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="sign-in-form-button"
+            >
+              Sign In
+            </Button>
+            Or <Link to={Routes.signUp}>Sign Up Here</Link>
+          </Form.Item>
+        </Form>
+      </Row>
+      <Row justify="center"></Row>
+    </AuthLayout>
+  );
+};
 
 export default SignInPage;
