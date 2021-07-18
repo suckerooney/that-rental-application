7ce4a322ace3b65a33a24f820068d93df6517b2b
Tomas Torres <tomas8torres@gmail.com>
2021-07-17 23:00:25 -0500

    added form loading

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -2,24 +2,44 @@ import "./SignInPage.less";
 import AuthLayout from "../../layouts/AuthLayout";
 import Routes from "../../common/Routes";
 import DisplayText from "../../components/DisplayText/DisplayText";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Row, Form, Input, Typography, Divider } from "antd";
+import { Button, Row, Form, Input, Typography, Divider, Spin } from "antd";
 import { MailOutlined, LockOutlined } from "@ant-design/icons";
 import GoogleButton from "react-google-button";
 import { EmailRules, PasswordRules } from "../../common/FormRules";
 
 const { Text } = Typography;
 
+// Delete
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 const SignInPage: FC = () => {
-  const onSubmit = (values: any) => {
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>();
+
+  const onFinish = async (values: any) => {
+    setLoading(true);
+    setError(null);
     console.log("Received values of form: ", values);
+    await delay(3000);
+    setLoading(false);
+    form.setFieldsValue({ password: "" });
+    setError("Invalid email or password");
   };
 
   return (
     <AuthLayout>
       <Row justify="center">
-        <Form name="sign-in" className="sign-in-form" onFinish={onSubmit}>
+        <Form
+          name="sign-in"
+          form={form}
+          className="sign-in-form"
+          onFinish={onFinish}
+        >
           <Form.Item>
             <DisplayText>Sign in</DisplayText>
           </Form.Item>
@@ -27,25 +47,37 @@ const SignInPage: FC = () => {
             <Input
               addonBefore={<MailOutlined className="form-input-icon" />}
               placeholder="Email"
+              disabled={loading}
             />
           </Form.Item>
           <Form.Item name="password" rules={PasswordRules}>
             <Input.Password
               addonBefore={<LockOutlined className="form-input-icon" />}
               placeholder="Password"
+              disabled={loading}
             />
           </Form.Item>
           <Form.Item>
             <Link to={Routes.signIn}>Forgot Password?</Link>
           </Form.Item>
+          {error && (
+            <Row justify="center">
+              <Text className="error-message" type="danger">
+                {error}
+              </Text>
+            </Row>
+          )}
           <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="sign-in-form-button"
-            >
-              Sign In
-            </Button>
+            <Spin spinning={loading} className="sign-in-form-button" />
+            {!loading && (
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="sign-in-form-button"
+              >
+                Sign In
+              </Button>
+            )}
             Or <Link to={Routes.signUp}>Sign up here</Link>
           </Form.Item>
           <Form.Item>
--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -10,9 +10,9 @@ import {
   PasswordCreationRules,
   ConfirmPasswordRules,
 } from "../../common/FormRules";
-import { FC } from "react";
+import { FC, useState, useRef } from "react";
 import { Link } from "react-router-dom";
-import { Button, Row, Typography, Form, Input, Divider } from "antd";
+import { Button, Row, Typography, Form, Input, Divider, Spin } from "antd";
 import {
   UserOutlined,
   MailOutlined,
@@ -23,15 +23,36 @@ import GoogleButton from "react-google-button";
 
 const { Text, Paragraph } = Typography;
 
+// Delete
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 const SignUpPage: FC = () => {
-  const onSubmit = (values: any) => {
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>();
+
+  const onFinish = async (values: any) => {
+    setLoading(true);
+    setErrorMessage(null);
     console.log("Received values of form: ", values);
+    await delay(3000);
+    setLoading(false);
+    setErrorMessage("email already in use");
+    form.setFieldsValue({ email: "" });
+    form.getFieldInstance("email").focus();
   };
 
   return (
     <AuthLayout>
       <Row justify="center">
-        <Form name="sign-up" className="sign-up-form" onFinish={onSubmit}>
+        <Form
+          name="sign-up"
+          form={form}
+          className="sign-up-form"
+          onFinish={onFinish}
+        >
           <Form.Item>
             <DisplayText>Sign up</DisplayText>
           </Form.Item>
@@ -40,18 +61,21 @@ const SignUpPage: FC = () => {
               addonBefore={<UserOutlined className="form-input-icon" />}
               type="text"
               placeholder="Name"
+              disabled={loading}
             />
           </Form.Item>
           <Form.Item name="email" validateTrigger="onBlur" rules={EmailRules}>
             <Input
               addonBefore={<MailOutlined className="form-input-icon" />}
               placeholder="Email"
+              disabled={loading}
             />
           </Form.Item>
           <Form.Item name="password" rules={PasswordCreationRules}>
             <Input.Password
               addonBefore={<LockOutlined className="form-input-icon" />}
               placeholder="Password"
+              disabled={loading}
             />
           </Form.Item>
           <Form.Item
@@ -62,16 +86,27 @@ const SignUpPage: FC = () => {
             <Input.Password
               addonBefore={<LockFilled className="form-input-icon" />}
               placeholder="Confirm Password"
+              disabled={loading}
             />
           </Form.Item>
+          {errorMessage && (
+            <Row justify="center">
+              <Text className="error-message" type="danger">
+                {errorMessage}
+              </Text>
+            </Row>
+          )}
           <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="sign-in-form-button"
-            >
-              Create Account
-            </Button>
+            <Spin spinning={loading} className="sign-up-form-button" />
+            {!loading && (
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="sign-up-form-button"
+              >
+                Create Account
+              </Button>
+            )}
             Or <Link to={Routes.signIn}>Sign in here</Link>
           </Form.Item>
           <Form.Item>
